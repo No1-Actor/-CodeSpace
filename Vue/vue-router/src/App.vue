f8c97e35dc291bae7457317e79c760582f061954
洪凯 <595286168@qq.com>
2023-12-04 21:41:43 +0800

    vue-router

---
+++ Vue/vue-router/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav>
+        <router-link to="/home">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
+</template>
+
+<style>
+    #app{
+    text-align: center;
+        font-size: 40px;
+    }
+</style>
---
+++ Vue/vue-router/src/homeChild/recommend.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <li>推荐列表1</li>
+        <li>推荐列表2</li>
+        <li>推荐列表3</li>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
