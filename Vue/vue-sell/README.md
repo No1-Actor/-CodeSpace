c896504f50a2e60c8f435ec18da5af37fec63918
洪凯 <595286168@qq.com>
2023-12-11 21:37:54 +0800

    vue-sell

---
+++ Vue/vue-sell/README.md
@@ -0,0 +1,7 @@
+# 项目开发
+1. 创建全局的css文件，添加reset.css
+
+# 组件化
+1. import 引入
+2. components注册
+3. 使用
---
+++ Vue/vue-sell/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-header/>
+  <router-view/>
+</template>
+
+<script>
+import Header from '@/components/header/Header.vue'
+export default {
+  components: { // 声明组件
+    'v-header': Header
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
