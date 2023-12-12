93f30f99b95eeb017b27882c23a8eb947006385b
洪凯 <595286168@qq.com>
2023-12-12 21:41:59 +0800

    vue-sell第三次更新

--- Vue/vue-sell/src/App.vue
+++ Vue/vue-sell/src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <v-header/>
-  <router-view/>
+  <v-header :seller="sellerData"/>
+
+  <router-view />
 </template>
 
 <script>
-import Header from '@/components/header/Header.vue'
+import Header from "@/components/header/Header.vue";
+import { getSeller } from "@/api";
 export default {
-  components: { // 声明组件
-    'v-header': Header
-  }
-}
+  components: {
+    // 声明组件
+    "v-header": Header,
+  },
+  data() {
+    return {
+      sellerData: {}
+    }
+  },
+  created() {
+    // 获取数据
+    getSeller().then((res) => {
+      console.log(res);
+      this.sellerData = res
+    });
+  },
+};
 </script>
 
 <style lang="less">
---
+++ Vue/vue-sell/src/components/header-detail/Header-detail.vue
@@ -0,0 +1,63 @@
+<template>
+  <transition name="fade">
+    <div class="header-detail">
+      <div class="detail-wrapepr">
+        <div class="detail-main">
+          <h1 class="name">店铺名称</h1>
+        </div>
+      </div>
+      <div class="detail-close" @click.stop="hideDetail">X</div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    hideDetail() { // 子父组件通讯
+      this.$emit('hide',false) // 创建一个hide事件 触发父组件的事件
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import "@/common/style/variable.less";
+.header-detail {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background: @color-background-s;
+  color: @color-white;
+  backdrop-filter: blur(10px); // 底层背景虚化
+  &.fade-enter-from,
+  &.fade-leave-to {
+    // 进入和离开的状态
+    opacity: 0; // 透明度
+    background: @color-background;
+  }
+  &.fade-enter-active,
+  &.fade-leave-active {
+    // 进入和离开的过渡动画
+    transition: all 0.5s ease; // 过渡动画 ease 逐渐变慢
+  }
+}
+
+.detail-close {
+  width: 30px;
+  height: 30px;
+  position: fixed;
+  bottom: 30px;
+  left: 50%;
+  margin-left: -15px;
+  text-align: center;
+  line-height: 30px;
+}
+</style>
