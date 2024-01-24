2daae9ff1761f750a1105f3b7c50572fdbeed61e
No1-Actor <595286168@qq.com>
2024-01-24 22:50:57 +0800

    music

--- Uniapp/music/components/wyheader/wyheader.vue
+++ Uniapp/music/components/wyheader/wyheader.vue
@@ -1,53 +1,54 @@
 <template>
-	<view class="head"></view>
-	<view class="header" :style="{ backgroundColor: bgColor }">
+	<view class="head" v-if="needBox"></view>
+	<view class="header" :style="{backgroundColor: bgColor}">
 		<uni-icons type="bars" size="22" @click="showMenu"></uni-icons>
-
+		
 		<view class="content">
 			<!-- 添加一个插槽 -->
 			<slot name="content"></slot>
 		</view>
-
+		
 		<uni-icons :type="icon" size="22"></uni-icons>
 	</view>
 	<view class="box" v-if="needBox"></view>
 </template>
 
 <script setup>
-import { useStore } from 'vuex';
-const store = useStore();
-
-defineProps({
-	icon: {
-		type: String,
-		default: 'mic'
-	},
-	bgColor: {
-		type: String,
-		default: '#fff'
-	},
-	needBox: {
-		type: Boolean,
-		default: true
+	import { useStore } from 'vuex';
+	const store = useStore()
+	
+	defineProps({
+		icon: {
+			type: String,
+			default: 'mic'
+		},
+		bgColor: {
+			type: String,
+			default: '#fff'
+		},
+		needBox: {
+			type: Boolean,
+			default: true
+		},
+		fontColor: String
+	})
+	
+	const showMenu = () => {
+		store.commit('changeIsShowMenu', true)
 	}
-});
-
-const showMenu = () => {
-	store.commit('changeIsShowMenu', true);
-};
 </script>
 
-<style lang="scss" scoped>	
+<style lang="scss" scoped>
 .head{
 	position: fixed;
 	top: 0;
 	left: 0;
-	height: 80rpx;
 	width: 100%;
-	z-index: 9999;
+	height: 80rpx;
 	background-color: #fff;
+	z-index: 9999;
 }
-.header {
+.header{
 	// background-color: #fff;
 	height: 100rpx;
 	display: flex;
@@ -60,8 +61,12 @@ const showMenu = () => {
 	width: 100%;
 	box-sizing: border-box;
 	z-index: 999;
+	color: v-bind(fontColor);
+	:deep(.uni-icons){
+		color: v-bind(fontColor) !important;
+	}
 }
-.box {
+.box{
 	height: 180rpx;
 }
 </style>
