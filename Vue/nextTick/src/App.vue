3a3ab5f32b4afce03ad0cb4f5a37041fa72b82b5
No1-Actor <595286168@qq.com>
2024-01-30 22:29:50 +0800

    nextTick

---
+++ Vue/nextTick/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <List></List>
+  </div>
+</template>
+
+<script setup>
+import List from './components/List.vue';
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ Vue/nextTick/src/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button @click="updateList">更新列表</button>
+        <ul>
+            <li v-for="n in list">{{ n }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { myNextTick } from './next-tick';
+
+const list = ref(new Array(20).fill(0))
+
+const updateList = () => {
+    list.value.push(...(new Array(10).fill(1)))
+
+    myNextTick = () => {
+        const liItem = document.querySelector('li:last-child')
+        liItem.scrollIntoView({ behavior: 'smooth' })
+    }
+}
+
+</script>
+
+<style lang="css" scoped>
+li{
+    height: 100px;
+    background-color: aquamarine;
+    margin: 10px;
+}
+</style>
