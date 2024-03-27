122600cc7d38fc025ae3dff2f2ba03f43f5ecc79
No1-Actor <595286168@qq.com>
2024-03-27 20:51:04 +0800

    v3-three

--- Vue/tesla/v3-three/README.md
+++ Vue/tesla/v3-three/README.md
@@ -15,4 +15,8 @@ canvas 3D html5 的绘制标签 数据可视化 游戏
 - echarts 2D
 - three.js 3D
     - 以导演的感性去构建3D世界
-    - 
+
+- 小米汽车APP 仿品
+- three.js canvas 数据可视化及web 3D渲染
+
+- 基于three.js 实现3D在线选车配件功能
--- Vue/tesla/v3-three/src/App.vue
+++ Vue/tesla/v3-three/src/App.vue
@@ -1,120 +1,283 @@
+<script setup>
+import * as THREE from "three";
+import { ref, onMounted } from "vue";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// canvas 3D  html5 的绘图标签  数据可视化，游戏
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+
+const containerRef = ref(null); // canvas
+
+// 相机
+
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight, // 横着拿
+  0.1,
+  1000
+);
+
+let controls = null;
+
+// x   y    z 相机放置的位置， 酷家乐  3D家居
+camera.position.set(0, 2, 6); // 摆好
+// 场景
+
+const scene = new THREE.Scene();
+// 渲染器 canvas 幕布 不停的去输出
+// OpenGL 3D建模 显卡计算 GPU  canvas 3D  GPU计算
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+
+renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
+const render = () => {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render); // 性能优化
+};
+
+onMounted(() => {
+  containerRef.value.appendChild(renderer.domElement);
+  renderer.setClearColor("#000");
+  scene.background = new THREE.Color("#fff");
+  scene.environment = new THREE.Color("#fff");
+  render();
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+  // 坐标辅助线
+  const gridHelper = new THREE.GridHelper(10, 10);
+  gridHelper.material.opacity = 0.2;
+  gridHelper.material.transparent = true;
+  scene.add(gridHelper);
+
+  // 加载汽车模型
+  const loader = new GLTFLoader(); // 3DMax gltf 格式
+  const dracoLoader = new DRACOLoader(); // 解码器
+  dracoLoader.setDecoderPath("../public/roadSter/draco/gltf/");
+  loader.setDRACOLoader(dracoLoader);
+  loader.load("../public/roadSter/model/bmw01.glb", (gltf) => {
+    const bmw = gltf.scene;
+    scene.add(bmw);
+    // 模型由各个部分构成的， 数组一样， 可以让我们遍历
+    bmw.traverse((child) => {
+      if (child.isMesh && child.name.includes("轮毂")) {
+        child.material = wheelsMaterial;
+        wheels.push(child);
+      }
+
+      if (child.isMesh && child.name.includes("Mesh002")) {
+        carBody = child;
+        carBody.material = bodyMaterial;
+      }
+
+      if (child.isMesh && child.name.includes("前脸")) {
+        frontCar = child;
+        frontCar.material = frontMaterial;
+      }
+
+      if (child.isMesh && child.name.includes("引擎盖_1")) {
+        hoodCar = child;
+        hoodCar.material = hoodMaterial;
+      }
+
+      if (child.isMesh && child.name.includes("挡风玻璃")) {
+        glassCar = child;
+        glassCar.material = glassMaterial;
+      }
+    });
+  });
+
+  const light1 = new THREE.DirectionalLight(0xffffff, 1);
+  light1.position.set(0, 0, 10);
+  scene.add(light1);
+
+  const light2 = new THREE.DirectionalLight(0xffffff, 1);
+  light2.position.set(0, 0, -10);
+  scene.add(light2);
+
+  const light3 = new THREE.DirectionalLight(0xffffff, 1);
+  light3.position.set(10, 0, 0);
+  scene.add(light3);
+
+  const light4 = new THREE.DirectionalLight(0xffffff, 1);
+  light4.position.set(-10, 0, 0);
+  scene.add(light4);
+
+  const light5 = new THREE.DirectionalLight(0xffffff, 1);
+  light5.position.set(0, 10, 0);
+  scene.add(light5);
+
+  const light6 = new THREE.DirectionalLight(0xffffff, 1);
+  light6.position.set(5, 10, 0);
+  scene.add(light6);
+
+  const light7 = new THREE.DirectionalLight(0xffffff, 1);
+  light7.position.set(0, 10, 5);
+  scene.add(light7);
+
+  const light8 = new THREE.DirectionalLight(0xffffff, 1);
+  light8.position.set(0, 10, -5);
+  scene.add(light8);
+
+  const light9 = new THREE.DirectionalLight(0xffffff, 1);
+  light9.position.set(-5, 10, 0);
+  scene.add(light9);
+});
+
+let wheels = [];
+
+let carBody, frontCar, hoodCar, glassCar;
+
+const wheelsMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0x424449,
+  metalness: 1,
+  roughness: 0.5,
+});
+
+// 材质
+
+const bodyMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0x424449,
+  metalness: 1,
+  roughness: 0.5,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+
+const frontMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0x424449,
+  metalness: 1,
+  roughness: 0.5,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+
+const hoodMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0x424449,
+  metalness: 1,
+  roughness: 0.5,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+
+const glassMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0xffffff,
+  metalness: 0,
+  roughness: 0,
+  transmission: 1,
+  transparent: true,
+});
+
+let colors = [
+  {
+    name: "冷光银",
+    color: "#424449",
+  },
+  {
+    name: "经典黑",
+    color: "black",
+  },
+  {
+    name: "深海蓝",
+    color: "#000f4a",
+  },
+  {
+    name: "中国红",
+    color: "#bd050f",
+  },
+  {
+    name: "珍珠白",
+    color: "white",
+  },
+];
+
+const selectColor = (color) => {
+  bodyMaterial.color.set(color);
+  wheelsMaterial.color.set(color);
+  frontMaterial.color.set(color);
+  hoodMaterial.color.set(color);
+};
+</script>
+
 <template>
   <div>
     <div class="header"></div>
+
     <div class="home">
       <div class="content-box">
         <div class="container" ref="containerRef"></div>
+      </div>
+
+      <div class="content">
+        <div class="words">选择车身颜色</div>
+
+        <div class="select">
+          <div
+            class="select-item"
+            v-for="(item, index) in colors"
+            :key="index"
+            @click="selectColor(item.color)"
+          >
+            <div
+              class="select-item-color"
+              :style="{ background: item.color }"
+            ></div>
+
+            <div class="select-item-color-name">{{ item.name }}</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup>
-import * as THREE from 'three'
-import { ref , onMounted} from 'vue'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
-
-const containerRef = ref(null) // canvas
-
-const camera = new THREE.PerspectiveCamera( // 透视相机
-  40, // 角度
-  window.innerWidth / window.innerHeight, // 宽高比 横着
-  0.1, // 近平面
-  1000 // 远平面
-) 
-
-let controls = null
-
-camera.position.set(0, 2, 6) // 设置相机位置
-
-const scene = new THREE.Scene() // 场景
-
-// OpenGL 3D建模 
-const renderer =  new THREE.WebGLRenderer({ // 渲染器 canvas 幕布
-  antialias: true, // 抗锯齿
-}) 
-
-renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8) // 设置渲染器尺寸
-
-const render = () => {  
-  renderer.render(scene, camera) // 渲染器渲染场景和相机
-  requestAnimationFrame(render) // 循环渲染(性能优化)
+<style lang="less" scoped>
+* {
+  margin: 0;
+  padding: 0;
 }
 
-onMounted(() => {
-  containerRef.value.appendChild(renderer.domElement) // 挂载canvas
-  renderer.setClearColor('#000') // 设置渲染器背景颜色
-  scene.background = new THREE.Color('#fff') // 设置场景背景色
-  scene.environment = new THREE.Color('#fff') // 设置场景环境色
-  render()
+.header {
+  width: 100vw;
+  height: 12vh;
+}
 
-  controls = new OrbitControls(camera, renderer.domElement)
-  controls.update()
+.home {
+  display: flex;
+  .content-box {
+    position: relative;
+    // border: 1px solid #e8eaed;
+  }
 
-  // 坐标辅助线
-  const gridHelper = new THREE.GridHelper(10, 10) // 网格辅助器
-  gridHelper.material.opacity = 0.2 // 网格透明度
-  gridHelper.material.transparent = true // 网格透明
-  scene.add(gridHelper)
+  .content {
+    margin: 10px auto;
+    .words {
+      font-size: 20px;
+      margin: 20px;
+    }
+  }
 
-  // 加载汽车模型
-  const loader = new GLTFLoader() // 加载器 3DMax gltf格式
-  const dracoloader = new DRACOLoader()
-  dracoloader.setDecoderPath('../public/roadSter/draco/gltf')
-  loader.setDRACOLoader(dracoloader)
-  loader.load('../public/roadSter/model/bmw01.glb', gltf => {
-      const bmw = gltf.scene
-      scene.add(bmw)
-      bmw.traverse(child => {
-        if(child.isMesh && child.name.includes("轮毂")) {
-          child.material = wheelsMasterial
-          child.material.envMapIntensity = 0.5
-        }
-      })
-  })
+  .select {
+    display: flex;
+  }
 
-  const light = new THREE.DirectionalLight(0xffffff, 1)
-  light.position.set(0, 0, 10)
-  scene.add(light)
-  const light2 = new THREE.DirectionalLight(0xffffff, 1);
-  light2.position.set(0, 0, -10);
-  scene.add(light2);
-  const light3 = new THREE.DirectionalLight(0xffffff, 1);
-  light3.position.set(10, 0, 0);
-  scene.add(light3);
-  const light4 = new THREE.DirectionalLight(0xffffff, 1);
-  light4.position.set(-10, 0, 0);
-  scene.add(light4);
-  const light5 = new THREE.DirectionalLight(0xffffff, 1);
-  light5.position.set(0, 10, 0);
-  scene.add(light5);
-  const light6 = new THREE.DirectionalLight(0xffffff, 1);
-  light6.position.set(5, 10, 0);
-  scene.add(light6);
-  const light7 = new THREE.DirectionalLight(0xffffff, 1);
-  light7.position.set(0, 10, 5);
-  scene.add(light7);
-  const light8 = new THREE.DirectionalLight(0xffffff, 1);
-  light8.position.set(0, 10, -5);
-  scene.add(light8);
-  const light9 = new THREE.DirectionalLight(0xffffff, 1);
-  light9.position.set(-5, 10, 0);
-  scene.add(light9);
+  .select-item {
+    margin: 5px;
+    cursor: pointer;
+  }
 
-})
+  .select-item-color {
+    width: 30px;
+    height: 30px;
+    border: 1px solid #ccc;
+    border-radius: 50px;
+    margin-bottom: 10px;
+  }
 
-let wheels = []
-
-const wheelsMasterial = new THREE.MeshStandardMaterial({
-  color: '0x424449',
-  metalness: 1,
-  roughness: 0.5,
-})
-
-</script>
-
-<style lang="css" scoped>
-
+  .select-item-color-name {
+    font-size: 14px;
+  }
+}
 </style>
